8ef1e4465a5f1837a228530f98a859e568cb1ec6
WindyRH156 <wendaybabang@gmail.com>
2024-03-17 11:47:28 +0700

    Revisi tampilan dan penambahan section

--- js/script.js
+++ js/script.js
@@ -18,6 +18,96 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// ---------------------------------------------------------------------------------
+
+// Animasi Slide
+
+var slideIndex = 1; // Tambahkan variabel slideIndex di luar fungsi currentDiv
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+function autoSlide() {
+  setInterval(function () {
+    slideIndex++;
+    if (slideIndex > document.getElementsByClassName("mySlides").length) {
+      slideIndex = 1;
+    }
+    showDivs(slideIndex);
+  }, 5000); // Ganti angka 5000 dengan interval auto slide dalam milidetik (misalnya 5000 untuk setiap 5 detik)
+}
+
+window.onload = function () {
+  autoSlide(); // Panggil fungsi autoSlide saat halaman dimuat
+};
+
+// ----------------------------------------------------------------------------------
+
+// Animasi mengetik
+
+const textArray = [
+  "Butuh jasa konveksi cepat murah dan terpercaya ?",
+  "ke IXDIM aja!",
+];
+const typingDelay = 50; // Delay antara tiap karakter (milidetik)
+const erasingDelay = 50; // Delay saat menghapus (milidetik)
+const newTextDelay = 600; // Delay sebelum mengetik kembali setelah selesai (milidetik)
+const pauseBetweenTexts = 2000; // Waktu jeda sebelum teks pertama dihapus (milidetik)
+let textArrayIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = textArray[textArrayIndex];
+  if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) {
+      textArrayIndex = 0;
+    }
+    setTimeout(type, newTextDelay);
+    return;
+  }
+
+  if (!isDeleting && charIndex < currentText.length) {
+    document.getElementById("typed-text").innerHTML +=
+      currentText.charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else if (isDeleting && charIndex > 0) {
+    document.getElementById("typed-text").innerHTML = currentText.substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(type, erasingDelay);
+  } else {
+    isDeleting = true;
+    setTimeout(type, pauseBetweenTexts); // Menambah waktu jeda sebelum penghapusan dimulai
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, newTextDelay);
+});
+
 // ----------------------------------------------------------------------------------
 
 // Animasi in/out
